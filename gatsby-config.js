5b9c7064507d35d831ae83c23c0a4ef0ceb63e03
kayehol <kaye.holanda@gmail.com>
2020-09-28 19:32:06 -0300

    fixin db

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
+        apiURL: process.env.API_URL || "https://bleep-strapi.herokuapp.com",
         contentTypes: [
           'post',
           'category',
