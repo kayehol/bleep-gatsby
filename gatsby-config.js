9cfb376784361b9ed564730000f1ef4d5be9a03f
kayehol <kaye.holanda@gmail.com>
2020-09-30 22:42:55 -0300

    teste

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/content/blog`,
       },
     },
     /*
