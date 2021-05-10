4cada4268f518b0e4ac89a07071a56689b11449b
Kaye Holanda <kaye.holanda@ofm.local>
2021-05-10 15:15:05 -0300

    testando fix error ssr

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,12 @@ module.exports = {
     `gatsby-plugin-netlify-cms`,
     "gatsby-plugin-slug",
     {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
